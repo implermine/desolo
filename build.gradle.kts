d34818ff94273b43a2ce83a1f22d3a2d753c721c
implermine <lkorea12345@gmail.com>
2025-06-01 21:16:57 +0900

    build: set test-container test env

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,10 +34,22 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
 
 
+    // ===
     // 테스트
+    // ===
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    
+    // TestContainers
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:postgresql")
+    
+    // 테스트에서도 PostgreSQL 드라이버 사용
+    testRuntimeOnly("org.postgresql:postgresql")
+    
+    // H2는 필요시에만 (특정 단위 테스트용)
     testRuntimeOnly("com.h2database:h2")
 
     // 테스트 코드용 Lombok 추가
