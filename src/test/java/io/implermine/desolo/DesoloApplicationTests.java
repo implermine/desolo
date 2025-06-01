e1b70bcb8124b194ce8cf9d396087a30170f08f9
implermine <lkorea12345@gmail.com>
2025-06-01 18:44:07 +0900

    test: add basic test for context load

---
+++ src/test/java/io/implermine/desolo/DesoloApplicationTests.java
@@ -0,0 +1,13 @@
+package io.implermine.desolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesoloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
